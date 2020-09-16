d84fc61ae0000a5dd7a7b194226fc78fb2f7d970
dengweiqiang <dengweiqiang66@163.com>
2020-09-16 23:58:53 +0800

    dev，格式化看看

--- src/main/java/top/dengwq/gitdemo/demo/User.java
+++ src/main/java/top/dengwq/gitdemo/demo/User.java
@@ -4,24 +4,26 @@ package top.dengwq.gitdemo.demo;
  * Created by dengweiqiang on 2020/9/16
  */
 public class User {
-  private  String   id;
-    private   String name;
-  private short age;
+    private String id;
+    private String name;
+    private short age;
 
-  // 添加get set 方法
-  public String getId() {
-    return id;
-  }
+    // 添加get set 方法
+    public String getId() {
+        return id;
+    }
 
-  public void setId(String id) {
-    this.id = id;
-  }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // 在次选择整理，添加空格啥的
 }
