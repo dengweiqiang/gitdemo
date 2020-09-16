e906da7680cd4dcfd94da41cda3e98c19db50367
dengweiqiang <dengweiqiang66@163.com>
2020-09-16 23:14:14 +0800

    dev分支提交User实体类

---
+++ src/main/java/top/dengwq/gitdemo/demo/User.java
@@ -0,0 +1,11 @@
+package top.dengwq.gitdemo.demo;
+
+/**
+ * Created by dengweiqiang on 2020/9/16
+ */
+public class User {
+  private  String   id;
+    private   String name;
+  private short age;
+
+}
