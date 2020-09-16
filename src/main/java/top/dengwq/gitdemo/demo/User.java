58b214de764c877ecdc79fcf756e1f0961597f27
dengweiqiang <dengweiqiang66@163.com>
2020-09-17 00:32:00 +0800

    dev，提交不格式化

--- src/main/java/top/dengwq/gitdemo/demo/User.java
+++ src/main/java/top/dengwq/gitdemo/demo/User.java
@@ -4,20 +4,20 @@ package top.dengwq.gitdemo.demo;
  * Created by dengweiqiang on 2020/9/16
  */
 public class User {
-    private String id;
-    private String name;
-    private short age;
+    private    String id;
+     private String name;   
+  private short age;
 
     // 添加get set 方法
     public String getId() {
         return id;
-    }
+    }    
 
     public void setId(String id) {
         this.id = id;
     }
 
-    public String getName() {
+    public String getName() {  
         return name;
     }
 
